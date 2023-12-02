4846ae25505c844736cf163c88ff40cabd32c240
dupto3 <upton33@gmail.com>
2023-12-02 18:05:33 -0500

    Upload BLS data cleaning script

---
+++ 1.cleaning_processing/data_bls_clean.py
@@ -0,0 +1,147 @@
+#--------------------------------------
+#
+#  Code adapted from https://github.com/jagman88/Clean-BLS-County-Level-Employment-Data 
+#
+#--------------------------------------
+
+import pandas as pd
+#import requests
+
+#------------------------------------------------------
+# Note: BLS responds with error if above code is used. The files were instead downloaded manually via browser as .txt files from https://download.bls.gov/pub/time.series/la.
+
+# Download and save .TXT files from BLS website into current directory
+
+# BLS_url = 'https://download.bls.gov/pub/time.series/la'
+
+#filenames = ['la.area',
+#              'la.data.0.CurrentU90-94', 'la.data.0.CurrentU95-99',
+#              'la.data.0.CurrentU00-04', 'la.data.0.CurrentU05-09',
+#              'la.data.0.CurrentU10-14','la.data.0.CurrentU15-19','la.data.0.CurrentU20-24']
+
+#for xx in filenames:
+#    dls = BLS_url+xx
+#    resp = requests.get(dls)
+
+#    output = open(xx+'.txt', 'wb')
+#    output.write(resp.content)
+#    output.close()
+#
+# Note: BLS responds with error if above code is used. The files were instead downloaded manually via browser as .txt files.
+#------------------------------------------------------
+
+# Import area information
+df_areas = pd.read_table('/Users/denise/Coding/Capstone_2023/Capstone Data/Raw/la.area.txt')
+df_areas = df_areas[['area_code', 'area_text']]
+
+# Only keep county information
+df_areas = df_areas.loc[df_areas['area_code'].str.contains('CN')]
+df_areas.reset_index(drop=True, inplace=True)
+
+# Rename columns
+df_areas.columns = ['area_code', 'countyname']
+
+# Get county and state information
+tmp = df_areas['countyname'].str.split(', ', expand=True)
+df_areas['countyname'] = tmp[0]
+df_areas['state'] = tmp[1]
+
+# Remove whitespace
+df_areas['area_code'] = df_areas['area_code'].map(lambda x: x.strip())
+df_areas['countyname'] = df_areas['countyname'].map(lambda x: x.strip())
+#df_areas['state'] = df_areas['state'].map(lambda x: x.strip())    # Doesn't work?
+
+#------------------------------------------------------
+def get_BLS_county_data(BLS_data_path, df_areas):
+    '''
+    BLS_data_path : path for the text file containing the BLS data
+    df_areas      : dataframe containing BLS information about counties/areas
+    '''
+    # Import area information
+    col_types = {'series_id': str, 'year': int, 'period': str, 'value': str, 'footnote_codes': str}
+    df_bls_county = pd.read_table(BLS_data_path, dtype=col_types)
+
+    # Remove white space from column names # Update
+    df_bls_county.rename(columns=lambda x: x.strip(), inplace=True)
+
+    #Convert column type
+    df_bls_county['series_id'] = df_bls_county['series_id'].astype(str)
+    df_bls_county['year'] = df_bls_county['year'].astype(int)
+    df_bls_county['period'] = df_bls_county['period'].astype(str)
+    df_bls_county['value'] = df_bls_county['value'].astype(str)
+    df_bls_county['footnote_codes'] = df_bls_county['footnote_codes'].astype(str)
+
+    # Remove white space from code..
+    df_bls_county['series_id'] = df_bls_county['series_id'].map(lambda x: x.strip())
+
+    # Convert 'value' to numeric (kind of slow...)
+    df_bls_county['value'] = df_bls_county['value'].apply(pd.to_numeric, errors='coerce')
+
+    # Get variable code
+    df_bls_county['var_code'] = df_bls_county['series_id'].str[-2:]
+
+    # Get area code
+    df_bls_county['series_id'] = df_bls_county['series_id'].astype(str).str[3:].str[:-2]
+
+    # Get FIPS code (as string to preserve initial zeros)
+    df_bls_county['FIPS'] = df_bls_county['series_id'].str[2:7]
+
+    #------------------------------------------------------------
+    # Only keep rows corresponding to counties
+    df_bls_county = df_bls_county.loc[df_bls_county['series_id'].str.contains('CN')]
+
+    # Drop columns, reset index
+    df_bls_county = df_bls_county[['series_id','year','period','value','var_code','FIPS']]
+    df_bls_county.reset_index(drop=True, inplace=True)
+
+    # Rename codes with variable names, rename columns
+    df_bls_county['var_code'] = df_bls_county['var_code'].map({'03': 'Unemployment_Rate', '04': 'Unemployment',
+                                                                 '05': 'Employment', '06': 'Labor_Force'})
+    df_bls_county.columns = ['area_code', 'year', 'month', 'value','variable_name', 'FIPS']
+
+    # Drop month 13 (I think this is the year average?)
+    df_bls_county = df_bls_county.loc[df_bls_county['month']!='M13']
+    # Convert month to numeric values
+    df_bls_county['month'] = pd.to_numeric(df_bls_county['month'].str[1:])
+
+    #------------------------------------------------------------
+    # Merge area names and data
+    df_bls_county = pd.merge(df_bls_county, df_areas, how='inner', on='area_code')
+
+    # Convert to wide-format table
+    df_bls_county = df_bls_county.pivot_table(values='value', index=['area_code', 'FIPS', 'state', 'countyname',
+                                                            'year', 'month'], columns='variable_name')
+    df_bls_county.reset_index(inplace=True)
+    df_bls_county.columns.name = None
+
+    #------------------------------------------------------------
+    print('Done!')
+
+    return df_bls_county
+
+#------------------------------------------------------------
+# Import all years of data
+df_unemp_90_94 = get_BLS_county_data('/Users/denise/Coding/Capstone_2023/Capstone Data/Raw/la.data.0.CurrentU90-94.txt', df_areas)
+df_unemp_95_99 = get_BLS_county_data('/Users/denise/Coding/Capstone_2023/Capstone Data/Raw/la.data.0.CurrentU95-99.txt', df_areas)
+df_unemp_00_04 = get_BLS_county_data('/Users/denise/Coding/Capstone_2023/Capstone Data/Raw/la.data.0.CurrentU00-04.txt', df_areas)
+df_unemp_05_09 = get_BLS_county_data('/Users/denise/Coding/Capstone_2023/Capstone Data/Raw/la.data.0.CurrentU05-09.txt', df_areas)
+df_unemp_10_14 = get_BLS_county_data('/Users/denise/Coding/Capstone_2023/Capstone Data/Raw/la.data.0.CurrentU10-14.txt', df_areas)
+df_unemp_15_19 = get_BLS_county_data('/Users/denise/Coding/Capstone_2023/Capstone Data/Raw/la.data.0.CurrentU15-19.txt', df_areas)
+df_unemp_20_24 = get_BLS_county_data('/Users/denise/Coding/Capstone_2023/Capstone Data/Raw/la.data.0.CurrentU20-24.txt', df_areas)
+
+#------------------------------------------------------------
+# Merge all year's data  # Updated
+df_unemp_county = df_unemp_90_94
+df_unemp_county = pd.concat([df_unemp_county, df_unemp_95_99])
+df_unemp_county = pd.concat([df_unemp_county, df_unemp_00_04])
+df_unemp_county = pd.concat([df_unemp_county, df_unemp_05_09])
+df_unemp_county = pd.concat([df_unemp_county, df_unemp_10_14])
+df_unemp_county = pd.concat([df_unemp_county, df_unemp_15_19])
+df_unemp_county = pd.concat([df_unemp_county, df_unemp_20_24])
+
+# Sort by year-month
+df_unemp_county = df_unemp_county.sort_values(by=['area_code', 'year', 'month'], axis=0)
+
+# Save to CSV
+df_unemp_county[['FIPS', 'state', 'countyname', 'year', 'month',
+       'Employment', 'Labor_Force', 'Unemployment', 'Unemployment_Rate']].to_csv('BLS_county_employment.csv', index=False)
